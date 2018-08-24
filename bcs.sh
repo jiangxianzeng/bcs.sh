9cd7b8238e2bb89e35c8960f5415ee9bee15e317
jiangxianzeng <jiangxianzeng@126.com>
2018-08-24 14:29:31 +0800

    add help

--- bcs.sh
+++ bcs.sh
@@ -1,3 +1,15 @@
 #!/bin/sh
 
+[ -z "$BCSSH_URL" ] && BCSSH_URL=https://raw.githubusercontent.com/jiangxianzeng/bcs.sh/master/sheets/
 
+opts=""
+input=""
+for o; do
+	input="$input $o"
+done
+
+query=$(echo "$input" | sed 's@ *$@@; s@^ *@@; s@ @/@; s@ @+@g')
+
+curl -s "${BCSSH_URL}"/"$query"
+
+exit 0
